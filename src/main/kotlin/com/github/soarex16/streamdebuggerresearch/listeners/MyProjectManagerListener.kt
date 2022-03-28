e433466365dad6215f97073fbc74912182f53a8f
GitHub Action <action@github.com>
2022-03-28 11:26:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/soarex16/streamdebuggerresearch/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/soarex16/streamdebuggerresearch/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.soarex16.streamdebuggerresearch.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.soarex16.streamdebuggerresearch.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/soarex16/streamdebuggerresearch/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.soarex16.streamdebuggerresearch.services
+
+import com.github.soarex16.streamdebuggerresearch.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
